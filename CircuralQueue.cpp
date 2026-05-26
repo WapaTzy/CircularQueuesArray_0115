87f2deb839bcc6c2f4ad9750723785959e5e8556
“WapaTzy” <wafarozanu22@gmail.com>
2026-05-26 10:51:07 +0700

    membuat 4 case

--- CircuralQueue.cpp
+++ CircuralQueue.cpp
@@ -125,29 +125,17 @@ int main()
     while (true)
     {
         try
-  
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values " << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
 
-            switch (ch)
-            {
-            case '1':
-            {
-                q.insert();
-                break;
-            }
-            case '2':
-            {
-                q.remove();
-                break;
-            }
-            case '3':
-            {
-                q.display();
-                break;
-            }
-            case '4':
-            {
-                return 0;
-            }
+   
             default:
             {
                 cout << "Invalid option!!!" << endl;
