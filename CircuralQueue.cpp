934e2e1e4503d05261d1a4d569019fb31107ab4c
“WapaTzy” <wafarozanu22@gmail.com>
2026-05-26 10:20:20 +0700

    first commit

---
+++ CircuralQueue.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace namespace std;
+
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+
+    void insert()
+    {
+        int num;
+        cout << "Enter a number: ";
+        cin >> num;
+        cout << endl;
+
+        //cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
+        else if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else if (REAR == max - 1)
+        {
+            REAR = 0;
+        }
+        else
+        {
+            REAR = REAR + 1;
+        }
