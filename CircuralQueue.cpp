7c46c99ae5c4b4bb1f602549d9e01587a2e933de
“WapaTzy” <wafarozanu22@gmail.com>
2026-05-26 10:43:45 +0700

    membiat private class

--- CircuralQueue.cpp
+++ CircuralQueue.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-using namespace namespace std;
+using namespace std;
 
+class Queues
 {
-private:
-    static const int max = 3;
-    int FRONT, REAR;
-    int queue_array[max];
 
 public:
     Queues()
@@ -27,16 +24,145 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
-        else if (FRONT == -1)
+
+        //cek apakah antrian kosong
+        if (FRONT == -1)
         {
             FRONT = 0;
             REAR = 0;
         }
-        else if (REAR == max - 1)
+        else
         {
-            REAR = 0;
+            //jika REAR berada di posisi terakhir array, maka kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    void remove()
+    {
+        //cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "\nQueue underflow\n";
+            return;
+        }
+
+        cout << "\nThe number deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        //cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
         }
         else
         {
-            REAR = REAR + 1;
+            //jika elemen FRONT berada di posisi terakhir array, maka kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
         }
+    }
+
+    void display()
+    {
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //cek apakah antrian kosong
+        if (FRONT_position == -1)
+        {
+            cout << "\nQueue is empty\n";
+            return;
+        }
+
+        cout << "\nElements in the circular queue are......\n";
+
+        //jika FRONT <= REAR, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+        else
+        {
+            //jika FRONT <= REAR, iterasi dari FRONT hingga akhir array
+            while (FRONT_position <= max - 1)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            FRONT_position = 0;
+
+            //iterasi dari awal array hingga REAR
+            while (FRONT_position <= REAR_position)
+            {
+                cout << queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values " << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
