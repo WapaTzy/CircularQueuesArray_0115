603df46b0de89cdc57df0e20af411d39dc167630
“WapaTzy” <wafarozanu22@gmail.com>
2026-05-26 10:50:28 +0700

    membuat menu

--- CircuralQueue.cpp
+++ CircuralQueue.cpp
@@ -116,3 +116,49 @@ public:
         cout << endl;
     }
 };
+
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+  
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
